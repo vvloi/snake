44c6e9a6e37901e67d67cc1bc4069c0d2a7b73cd
Nguyen Quoc Dat <24730020@ms.uit.edu.vn>
2024-11-17 12:10:37 +0700

    add file types

---
+++ src/types.h
@@ -0,0 +1,97 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <time.h>
+
+typedef enum
+{
+    left,
+    right,
+    up,
+    down
+} Direction;
+typedef enum
+{
+    standard,
+    value,
+    hidden
+} FoodType;
+typedef enum
+{
+    slow,
+    fast,
+    life
+} Modifier;
+typedef enum
+{
+    stdStart,
+    stdEnd,
+    valStart,
+    valEnd,
+    hidStart,
+    hidEnd
+} DelayValues;
+typedef enum
+{
+    hiscore,
+    lives,
+    score
+} StatsValues;
+typedef enum
+{
+    startGame,
+    settings,
+    exitGame,
+    resumeGame
+} Menu;
+
+typedef struct
+{
+    int x;
+    int y;
+} Position;
+
+typedef struct
+{
+    Position pos;
+    Position oldPos;
+} EndPos;
+
+typedef struct
+{
+    EndPos head;
+    EndPos tail;
+    Position *seg;
+    int segCount;
+    int allocatedMemory;
+    int speed;
+    Direction dir;
+} Snake;
+
+typedef struct
+{
+    Position pos;
+    time_t time;
+    Modifier mod;
+    FoodType ftype;
+} Eats;
+
+typedef struct
+{
+    Eats standard[15];
+    Eats value;
+    Eats hidden;
+    int stdCount, valCount, hidCount;
+    int foodToAdd;
+    int maxFood;
+} Food;
+
+typedef struct
+{
+    time_t begin, end;
+    time_t elapsed;
+    time_t resetStd, resetVal, resetHid, resetSpeed, resetLife;
+    int delay[6];
+} Timer;
+
+#endif /* TYPES_H */
